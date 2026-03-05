6aaba8eb0b31adc558c032d0b9fb7b30ab81007d
AbhinandRajeev <abhinandrajeev10@gmail.com>
2026-03-05 22:03:04 +0530

    third commit

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
 // export const serverURL = "http://localhost:3000"
-export const serverURL = "https://devconnect-backend-dvy5.onrender.com/"
+export const serverURL = "https://devconnect-backend-dvy5.onrender.com"
